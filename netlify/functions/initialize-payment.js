33b24a09295a6314e3763ee667c31d55edbdf670
ScanzyBwoy123 <scanzybwoy8@gmail.com>
2026-08-16 08:54:56 +0000

    Update initialize-payment.js

--- netlify/functions/initialize-payment.js
+++ netlify/functions/initialize-payment.js
@@ -67,16 +67,15 @@ exports.handler = async (event) => {
           "Content-Type": "application/json"
         },
         body: JSON.stringify({
-          email,
-          amount: Math.round(amount * 100),
-          currency: "GHS",
-          reference,
-          metadata: {
-            plan
-          }
-        })
-      }
-    );
+  email,
+  amount: Math.round(amount * 100),
+  currency: "GHS",
+  reference,
+  callback_url: "https://palseprep1.netlify.app/",
+  metadata: {
+    plan
+  }
+})
 
     const data = await response.json();
 
